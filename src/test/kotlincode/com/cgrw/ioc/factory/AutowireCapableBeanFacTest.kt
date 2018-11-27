ceea5306b59da98216c9cc835f19f7a59e9ffdf7
cgIIrw <chenguo_jay@163.com>
2018-11-28 06:21:29 +0800

    完成依赖注入功能测试

--- src/test/kotlincode/com/cgrw/ioc/factory/AutowireCapableBeanFacTest.kt
+++ src/test/kotlincode/com/cgrw/ioc/factory/AutowireCapableBeanFacTest.kt
@@ -5,6 +5,7 @@ import kotlincode.com.cgrw.ioc.xml.XmlBeanDefinitionReader
 import org.junit.Test
 
 import org.junit.Assert.*
+import testresources.beans.Person
 import testresources.beans.Student
 
 /**
@@ -18,8 +19,10 @@ class AutowireCapableBeanFacTest {
     @Test
     fun doCreateBean() {
         xmlBeanDefinitionReader.loadBeanDefinitions("bean-config.xml")
-        var s = xmlBeanDefinitionReader.registry!!.get("student")!!
-        autowire.registerBeanDef("student", s)
-        println(autowire.doCreateBean(s) is Student)
+        var s = xmlBeanDefinitionReader.registry!!.get("person")!!
+        var s1 = xmlBeanDefinitionReader.registry!!.get("student")!!
+        autowire.registerBeanDef("person", s)
+        autowire.registerBeanDef("student", s1)
+        println((autowire.doCreateBean(s) as Person).refer is Student)
     }
 }
