47f5fa16305a167b9940ccf7ed921c0d0c5f0edb
cgIIrw <chenguo_jay@163.com>
2018-12-12 17:52:24 +0800

    更新AspectJ的切点类和切面类

--- src/main/kotlincode/com/cgrw/aop/AspectJExpressionPointcut.kt
+++ src/main/kotlincode/com/cgrw/aop/AspectJExpressionPointcut.kt
@@ -9,56 +9,79 @@ import java.lang.reflect.Method
  * @author: cgrw
  **/
 class AspectJExpressionPointcut : Pointcut, ClassFilter, MethodMatcher {
+
+    // 切点解析器
     var pointcutParser: PointcutParser? = null
 
+    // 表达式字符串
     var expression: String? = null
 
+    // 切点表达式
     var pointcutExpression: PointcutExpression? = null
 
     companion object {
-        private val DEFAULT_SUPPORTED_PRIMITIVES = HashSet<PointcutPrimitive>()
+
+        // 默认的primitives(原语)
+        private val primitives = HashSet<PointcutPrimitive>()
 
         init {
-            DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.EXECUTION)
-            DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.ARGS)
-            DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.REFERENCE)
-            DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.THIS)
-            DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.TARGET)
-            DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.WITHIN)
-            DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.AT_ANNOTATION)
-            DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.AT_WITHIN)
-            DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.AT_ARGS)
-            DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.AT_TARGET)
+            primitives.add(PointcutPrimitive.ADVICE_EXECUTION)
+            primitives.add(PointcutPrimitive.ARGS)
+            primitives.add(PointcutPrimitive.CALL)
+            primitives.add(PointcutPrimitive.EXECUTION)
+            primitives.add(PointcutPrimitive.GET)
+            primitives.add(PointcutPrimitive.HANDLER)
+            primitives.add(PointcutPrimitive.INITIALIZATION)
+            primitives.add(PointcutPrimitive.PRE_INITIALIZATION)
+            primitives.add(PointcutPrimitive.SET)
+            primitives.add(PointcutPrimitive.STATIC_INITIALIZATION)
+            primitives.add(PointcutPrimitive.TARGET)
+            primitives.add(PointcutPrimitive.THIS)
+            primitives.add(PointcutPrimitive.WITHIN)
+            primitives.add(PointcutPrimitive.WITHIN_CODE)
+            primitives.add(PointcutPrimitive.AT_ANNOTATION)
+            primitives.add(PointcutPrimitive.AT_THIS)
+            primitives.add(PointcutPrimitive.AT_TARGET)
+            primitives.add(PointcutPrimitive.AT_ARGS)
+            primitives.add(PointcutPrimitive.AT_WITHIN)
+            primitives.add(PointcutPrimitive.AT_WITHINCODE)
+            primitives.add(PointcutPrimitive.REFERENCE)
         }
     }
 
     constructor(supportedPrimitives: Set<PointcutPrimitive>) {
-        var pointcutParser = PointcutParser.getPointcutParserSupportingSpecifiedPrimitivesAndUsingContextClassloaderForResolution(supportedPrimitives)
+        pointcutParser = PointcutParser.getPointcutParserSupportingSpecifiedPrimitivesAndUsingContextClassloaderForResolution(supportedPrimitives)
     }
 
-    constructor() : this(DEFAULT_SUPPORTED_PRIMITIVES)
+    constructor() : this(primitives)
 
+    // 避免重复解析
     fun checkReadyToMatch() {
         pointcutExpression = pointcutExpression ?: buildPointcutExpression()
     }
 
+    // 切点解析器内部的解析操作
     private fun buildPointcutExpression(): PointcutExpression {
         return pointcutParser!!.parsePointcutExpression(expression)
     }
 
+    // 类过滤方法
     override fun getClassFilter(): ClassFilter {
         return this
     }
 
+    // 方法匹配方法
     override fun getMethodMatcher(): MethodMatcher {
         return this
     }
 
+    // 已经通过了判空，如果还为空将抛出空指针异常，所以双感叹号；
     override fun matches(targetClass: Class<*>): Boolean {
         checkReadyToMatch()
         return pointcutExpression!!.couldMatchJoinPointsInType(targetClass)
     }
 
+    // 已经通过了判空，如果还为空将抛出空指针异常，所以双感叹号；
     override fun matches(method: Method, targetClass: Class<*>): Boolean {
         checkReadyToMatch()
         var shadowMatch = pointcutExpression!!.matchesAdviceExecution(method)
--- src/main/kotlincode/com/cgrw/aop/AspectJExpressionPointcutAdvisor.kt
+++ src/main/kotlincode/com/cgrw/aop/AspectJExpressionPointcutAdvisor.kt
@@ -6,12 +6,19 @@ import org.aopalliance.aop.Advice
  * @author: cgrw
  **/
 
+// AspectJ切面
 class AspectJExpressionPointcutAdvisor : PointcutAdvisor {
     private var pointcut = AspectJExpressionPointcut()
+
+    // 这里为了兼容原本的get方法和重写的get方法，将属性设置为private
     private var advice: Advice? = null
 
     fun setAdvice(advice: Advice) {
         this.advice = advice
+    }
+
+    fun setExpression(expression: String) {
+        this.pointcut.expression = expression
     }
 
     override fun getPointcut(): Pointcut {
