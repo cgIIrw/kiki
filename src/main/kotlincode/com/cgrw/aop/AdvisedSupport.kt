d7f3872d0e67a7b286ba351382f75648bfbeb088
cgIIrw <chenguo_jay@163.com>
2018-12-14 07:30:06 +0800

    修改JDK动态代理相关代码，完善代理逻辑

--- src/main/kotlincode/com/cgrw/aop/AdvisedSupport.kt
+++ src/main/kotlincode/com/cgrw/aop/AdvisedSupport.kt
@@ -5,6 +5,7 @@ import org.aopalliance.intercept.MethodInterceptor
 /**
  * @author: cgrw
  **/
+// 通知支持，包含了方法匹配，拦截器对象以及目标对象包
 class AdvisedSupport {
     // 被代理的目标对象包
     var targetSource: TargetSource? = null
--- src/main/kotlincode/com/cgrw/aop/JdkDynamicAopProxy.kt
+++ src/main/kotlincode/com/cgrw/aop/JdkDynamicAopProxy.kt
@@ -9,14 +9,21 @@ import java.lang.reflect.Proxy
  **/
 class JdkDynamicAopProxy(var advised: AdvisedSupport) : AopProxy, InvocationHandler{
 
-
     override fun getProxy(): Any? {
-        return Proxy.newProxyInstance(this.javaClass.classLoader, arrayOf(advised.targetSource!!.targetClass), this)
+        return Proxy.newProxyInstance(this.javaClass.classLoader, advised.targetSource!!.targetClass, this)
 
     }
 
     override fun invoke(proxy: Any?, method: Method?, args: Array<Any>?): Any? {
         var methodInterceptor = advised.methodInterceptor
-        return methodInterceptor?.let { it.invoke(ReflectiveMethodInvocation(advised.targetSource?.let { it.target }, method, args))}
+        var methodMatcher = advised.methodMatcher
+
+        if (methodMatcher != null && methodMatcher.matches(method!!, advised.targetSource!!.target!!::class.java))
+            // 匹配时返回拦截器的处理方法
+            return methodInterceptor?.invoke(ReflectiveMethodInvocation(advised.targetSource!!.target, method, args))
+        else {
+            // 不匹配时返回目标对象的原本方法
+            return method?.invoke(advised.targetSource!!.target, args)
+        }
     }
 }
