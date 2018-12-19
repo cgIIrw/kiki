1d4bc62d130138706d48b63fa262d8b78d5021ee
cgIIrw <chenguo_jay@163.com>
2018-12-19 16:19:47 +0800

    创建环绕拦截器

--- src/main/testresources/beans/SimpleInterceptor.kt
+++ src/main/testresources/beans/SimpleInterceptor.kt
@@ -1,5 +1,17 @@
 package testresources.beans
 
+import org.aopalliance.intercept.MethodInterceptor
+import org.aopalliance.intercept.MethodInvocation
+
 /**
  * @author: cgrw
  **/
+
+public class SimpleInterceptor : MethodInterceptor {
+    override fun invoke(invocation: MethodInvocation?): Any? {
+        println("Before ...")
+        var rval = invocation!!.proceed()
+        println("After ...")
+        return rval
+    }
+}
