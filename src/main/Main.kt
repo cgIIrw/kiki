2b5c0cca1847823a6a2ccbfdf65ef6c9d3a29cc2
cgIIrw <chenguo_jay@163.com>
2018-12-19 16:23:49 +0800

    创建Main方法

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -1,3 +1,12 @@
+import kotlincode.com.cgrw.ioc.context.ClassPathXmlApplicationContext
+import testresources.beans.HelloJDKFather
+import testresources.beans.HelloJDKProxy
+
 /**
  * @author: cgrw
  **/
+
+fun main(args: Array<String>) {
+    var context = ClassPathXmlApplicationContext("bean-config.xml")
+    (context.getBean("helloJDKProxy") as HelloJDKProxy).helloMethod(3)
+}
