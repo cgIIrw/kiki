91b4e166e30382068ea13a312e276dc85ee2f3f8
cgIIrw <chenguo_jay@163.com>
2018-12-19 19:39:44 +0800

    测试CglibAopProxy

--- src/test/kotlincode/com/cgrw/aop/CglibAopProxyTest.kt
+++ src/test/kotlincode/com/cgrw/aop/CglibAopProxyTest.kt
@@ -3,6 +3,9 @@ package kotlincode.com.cgrw.aop
 import org.junit.Test
 
 import org.junit.Assert.*
+import testresources.beans.HelloJDKFather
+import testresources.beans.HelloJDKProxy
+import testresources.beans.SimpleInterceptor
 
 /**
  * @author: cgrw
@@ -11,5 +14,22 @@ class CglibAopProxyTest {
 
     @Test
     fun intercept() {
+
+        var helloJDKProxy = HelloJDKProxy()
+        var aspectJExpressionPointcut = AspectJExpressionPointcut()
+        var expression = "execution(* testresources.beans.*.*(..))"
+        var advisedSupport = AdvisedSupport()
+        var simpleInterceptor = SimpleInterceptor()
+
+        aspectJExpressionPointcut.expression = expression
+        var targetSource = TargetSource(helloJDKProxy, *helloJDKProxy.javaClass.interfaces)
+        var methodMatcher = aspectJExpressionPointcut.getMethodMatcher()
+
+        advisedSupport.targetSource = targetSource
+        advisedSupport.methodMatcher = methodMatcher
+        advisedSupport.methodInterceptor = simpleInterceptor
+
+        var CglibAopProxy = CglibAopProxy(advisedSupport).getProxy() as HelloJDKProxy
+        CglibAopProxy.helloMethod(5)
     }
 }
