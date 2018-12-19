dc7f07e2fa10a0259620570ecb208119af3003e5
cgIIrw <chenguo_jay@163.com>
2018-12-19 16:20:41 +0800

    测试TargetSource

--- src/test/kotlincode/com/cgrw/aop/TargetSourceTest.kt
+++ src/test/kotlincode/com/cgrw/aop/TargetSourceTest.kt
@@ -3,14 +3,19 @@ package kotlincode.com.cgrw.aop
 import org.junit.Test
 
 import org.junit.Assert.*
+import testresources.beans.HelloJDKProxy
 
 /**
  * @author: cgrw
  */
 class TargetSourceTest {
 
+    var helloJDKProxy = HelloJDKProxy()
+
+    var targetSource = TargetSource(helloJDKProxy, *helloJDKProxy.javaClass.interfaces)
     @Test
     fun getTargetClass() {
+        println(targetSource.targetClass[1].name)
     }
 
     @Test
