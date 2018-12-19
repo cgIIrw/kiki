1bc7b889fa2ab7c24823eaa4caf34fa0d7cbe70e
cgIIrw <chenguo_jay@163.com>
2018-12-19 16:21:21 +0800

    测试JDK动态代理

--- src/test/kotlincode/com/cgrw/aop/JdkDynamicAopProxyTest.kt
+++ src/test/kotlincode/com/cgrw/aop/JdkDynamicAopProxyTest.kt
@@ -2,7 +2,9 @@ package kotlincode.com.cgrw.aop
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import testresources.beans.HelloJDKFather
+import testresources.beans.HelloJDKProxy
+import testresources.beans.SimpleInterceptor
 
 /**
  * @author: cgrw
@@ -11,5 +13,22 @@ class JdkDynamicAopProxyTest {
 
     @Test
     fun getProxy() {
+        var helloJDKProxy = HelloJDKProxy()
+        var aspectJExpressionPointcut = AspectJExpressionPointcut()
+        var expression = "execution(* testresources.beans.*.*(..))"
+        var advisedSupport = AdvisedSupport()
+        var simpleInterceptor = SimpleInterceptor()
+
+
+        aspectJExpressionPointcut.expression = expression
+        var targetSource = TargetSource(helloJDKProxy, *helloJDKProxy.javaClass.interfaces)
+        var methodMatcher = aspectJExpressionPointcut.getMethodMatcher()
+
+        advisedSupport.targetSource = targetSource
+        advisedSupport.methodMatcher = methodMatcher
+        advisedSupport.methodInterceptor = simpleInterceptor
+
+        var jdkDynamicAopProxy = JdkDynamicAopProxy(advisedSupport).getProxy() as HelloJDKFather
+        jdkDynamicAopProxy.helloMethod(5)
     }
 }
