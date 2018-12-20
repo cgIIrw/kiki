5a7d3a4728c9215407c9975552bd37c858c1c7b2
cgIIrw <chenguo_jay@163.com>
2018-12-20 15:59:56 +0800

    去除AspectJExpressionPointcutAdvisor中重写的方法

--- src/main/kotlincode/com/cgrw/aop/AspectJExpressionPointcutAdvisor.kt
+++ src/main/kotlincode/com/cgrw/aop/AspectJExpressionPointcutAdvisor.kt
@@ -8,24 +8,23 @@ import org.aopalliance.aop.Advice
 
 // AspectJ切面
 class AspectJExpressionPointcutAdvisor : PointcutAdvisor {
-    private var pointcut = AspectJExpressionPointcut()
 
-    // 这里为了兼容原本的get方法和重写的get方法，将属性设置为private
-    private var advice: Advice? = null
+    var pointcut: AspectJExpressionPointcut? = null
+    var advice: Advice? = null
 
-    fun setAdvice(advice: Advice) {
-        this.advice = advice
-    }
-
-    fun setExpression(expression: String) {
-        this.pointcut.expression = expression
-    }
-
-    override fun getPointcut(): Pointcut {
+    override fun getMyPointcut(): Pointcut? {
         return pointcut
     }
 
-    override fun getAdvice(): Advice? {
+    override fun getMyAdvice(): Advice? {
         return advice
     }
+
+//    override fun getPointcut(): Pointcut? {
+//        return pointcut
+//    }
+//
+//    override fun getAdvice(): Advice? {
+//        return advice
+//    }
 }
