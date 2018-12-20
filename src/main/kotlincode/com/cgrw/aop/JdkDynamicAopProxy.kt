984d958056b687e8d683cda23f299a0b092a89c2
cgIIrw <chenguo_jay@163.com>
2018-12-20 15:58:20 +0800

    修正JdkDynamicAopProxy中invoke参数错误问题

--- src/main/kotlincode/com/cgrw/aop/JdkDynamicAopProxy.kt
+++ src/main/kotlincode/com/cgrw/aop/JdkDynamicAopProxy.kt
@@ -7,11 +7,11 @@ import java.lang.reflect.Proxy
 /**
  * @author: cgrw
  **/
-class JdkDynamicAopProxy(var advised: AdvisedSupport) : AopProxy, InvocationHandler{
+class JdkDynamicAopProxy(var advised: AdvisedSupport) : AopProxy, InvocationHandler {
 
     override fun getProxy(): Any? {
-        return Proxy.newProxyInstance(advised.targetSource!!.target!!.javaClass.classLoader, advised.targetSource!!.targetClass, this)
-
+        return Proxy.newProxyInstance(advised.targetSource!!.target!!
+                .javaClass.classLoader, advised.targetSource!!.targetClass, this)
     }
 
     override fun invoke(proxy: Any?, method: Method?, args: Array<Any>?): Any? {
@@ -19,11 +19,11 @@ class JdkDynamicAopProxy(var advised: AdvisedSupport) : AopProxy, InvocationHand
         var methodMatcher = advised.methodMatcher
 
         if (methodMatcher != null && methodMatcher.matches(method!!, advised.targetSource!!.target!!::class.java))
-            // 匹配时返回拦截器的处理方法
+        // 匹配时返回拦截器的处理方法
             return methodInterceptor?.invoke(ReflectiveMethodInvocation(advised.targetSource!!.target, method, args))
         else {
             // 不匹配时返回目标对象的原本方法
-            return method?.invoke(advised.targetSource!!.target, args)
+            return method?.invoke(advised.targetSource!!.target, *(args ?: arrayOfNulls<Any>(0)))
         }
     }
 }
