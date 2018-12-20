c717213af8762c8d48b3ed6c413993c525511697
cgIIrw <chenguo_jay@163.com>
2018-12-20 15:59:04 +0800

    添加CglibAopProxy动态代理类

--- src/main/kotlincode/com/cgrw/aop/CglibAopProxy.kt
+++ src/main/kotlincode/com/cgrw/aop/CglibAopProxy.kt
@@ -8,22 +8,25 @@ import java.lang.reflect.Method
 /**
  * @author: cgrw
  **/
-
 class CglibAopProxy(var advised: AdvisedSupport) : MethodInterceptor, AopProxy {
 
+    // 被代理的目标对象
     var targetObject = advised.targetSource!!.target
 
+    // 获取创建的代理对象
     override fun getProxy(): Any? {
         return createProxyInstance()
     }
 
-    fun createProxyInstance(): Any? {
+    // 创建代理对象的方法
+    private fun createProxyInstance(): Any? {
         var enhancer = Enhancer()
         enhancer.setSuperclass(this.targetObject!!::class.java)
         enhancer.setCallback(this)
         return enhancer.create()
     }
 
+    // 代理方法的逻辑
     override fun intercept(p0: Any?, p1: Method?, p2: Array<Any>?, p3: MethodProxy?): Any? {
         var methodInterceptor = advised.methodInterceptor
         var methodMatcher = advised.methodMatcher
@@ -33,7 +36,7 @@ class CglibAopProxy(var advised: AdvisedSupport) : MethodInterceptor, AopProxy {
             return methodInterceptor?.invoke(ReflectiveMethodInvocation(advised.targetSource!!.target, p1, p2))
         else {
             // 不匹配时返回目标对象的原本方法
-            return p1?.invoke(advised.targetSource!!.target, p2)
+            return p1?.invoke(advised.targetSource!!.target, *(p2 ?: arrayOfNulls<Any>(0)))
         }
     }
 }
